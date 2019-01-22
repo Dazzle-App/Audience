dce6a9aecbbf724531a1ed68cc575308f7a81adf
Andrew Huggins <andrewhuggins@gmail.com>
2019-01-21 21:15:13 -0500

    The new config file

---
+++ src/bootstrap/config/audience.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Config For the DazzleApp\Audience package
+ */
+return [
+
+    'admin_user' => env('AUDIENCE_USER'),
+
+    'admin_email' => env('AUDIENCE_EMAIL'),
+
+    'admin_password' => env('AUDIENCE_PASSWORD')
+
+];
