b3b64dbd02cc2d170118266b3e3fd1cf8fc06eba
Andrew Huggins <andrewhuggins@gmail.com>
2019-01-21 21:15:27 -0500

    add routes for Create on AudienceController

--- src/bootstrap/routes/routes.php
+++ src/bootstrap/routes/routes.php
@@ -4,6 +4,7 @@ Route::get('audience/confirm/{confirm_code}', function ($confirm_code) {
     dd('works', $confirm_code);
 });
 
-Route::get('audience', 'DazzleApp\Audience\Controllers\AudienceController@form');
+Route::get('audience', 'DazzleApp\Audience\Controllers\AudienceController@form')->middleware(['web']);
+Route::post('audience', 'DazzleApp\Audience\Controllers\AudienceController@create')->name('audience');
 
 Route::post('someroute', 'DazzleApp\Audience\Controllers\AudienceController@test');
