61ed73c73b3526bbb1ec630dd130261f65660920
Andrew Huggins <andrewhuggins@gmail.com>
2019-01-21 21:15:05 -0500

    add publishes to ServiceProvider to handle config

--- src/Providers/DazzleAppAudienceServiceProvider.php
+++ src/Providers/DazzleAppAudienceServiceProvider.php
@@ -14,15 +14,19 @@ class DazzleAppAudienceServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('audienceSignUp', function ($expression) {
-            return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";
+        Blade::directive('audienceSignUp', function () {
+            return view('audience::form');
         });
 
         $this->loadMigrationsFrom(__DIR__ . '/../bootstrap/migrations');
 
-        $this->loadRoutesFrom(__DIR__.'/../bootstrap/routes/routes.php');
+        $this->loadRoutesFrom(__DIR__ . '/../bootstrap/routes/routes.php');
 
-        $this->loadViewsFrom(__DIR__.'/../views', 'audience');
+        $this->loadViewsFrom(__DIR__ . '/../views', 'audience');
+
+        $this->publishes([
+            __DIR__ . '/../bootstrap/config/audience.php' => config_path('audience.php'),
+        ]);
     }
 
     /**
