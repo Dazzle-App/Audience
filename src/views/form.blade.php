7610c0524bcc598c1197c1ae58b2dd79eed3d4f9
Andrew Huggins <andrewhuggins@gmail.com>
2019-01-21 21:15:34 -0500

    a simple form

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,1 +1,9 @@
-@audienceSignUp
+<form method="POST" action="{{ route('audience') }}">
+    @csrf
+    <div>
+        <input name="email" id="type" type="email" value="{{ old('email') }}">
+    </div>
+    <div>
+        <button class="">Sign Up</button>
+    </div>
+</form>
